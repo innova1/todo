e059db20a4bdb092762b9af3758cd7fc9ca460a8
innova1 <tom@teaminnovationllc.com>
2018-11-19 15:58:54 -0500

    update controller

--- controllers/showController.js
+++ controllers/showController.js
@@ -4,7 +4,7 @@ const os = require("os");
 const debug = require('debug')('app:showController');
 
 
-exports.showTasks = async function (req, res) {
+exports.showFbks = async function (req, res) {
   try {
     const dbParams = await util.setupDB();
     const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
