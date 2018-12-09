a2d4d0cf1b8fd9981bca47264f141b68e5c3245a
innova1 <tom@teaminnovationllc.com>
2018-12-08 20:33:56 -0500

    moved to loginsContr..

---
+++ controllers/loginsController.js
@@ -0,0 +1,22 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const util = require('./utilController');
+const debug = require('debug')('app:logincontroller');
+
+exports.loginPage = (req, res) => {
+    res.render('loginPage', { title: 'Find your name and enter your password' });
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = req.body;
+    const dbParams = await util.setupUserDB();
+    await dbParams.collection.insertOne(user);
+    dbParams.client.close();
+    res.redirect('/');
+  }
+
+    
+  catch(err) {
+    debug(err);
+  }
+};
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -20,8 +20,8 @@ router.get('/task/add/', addController.addTask);
 router.get('/task/add2/', addController.addFbk);
 router.post('/task/add/', addController.saveTask);
 router.get('/export', exportController.exportFbks);
-router.get('/login', logincontroller.loginPage);
-router.post('/login', logincontroller.login);
+router.get('/login', loginsController.loginPage);
+router.post('/login', loginsController.login);
 router.get('/', showController.showFbks);
 
 module.exports = router;
