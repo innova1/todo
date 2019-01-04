c9fe1e4634ee0cb4d6a3bfe36d51a96c17381bd5
innova1 <tom@teaminnovationllc.com>
2019-01-04 15:53:07 -0500

    fix export

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -7,9 +7,9 @@ const debug = require('debug')('app:showController');
 exports.exportFbks = async function (req, res) {
   try {
     const dbParams = await util.setupDB();
-    const tasks = await dbParams.collection.find({}).sort({ dueDate: -1 }).toArray();
+    const fbks = await dbParams.collection.find({}).sort({ dueDate: -1 }).toArray();
     const hostname = os.hostname();
-    res.render('exportFbks', { tasks, title: 'Export of Feedback List', hostname });
+    res.render('exportFbks', { fbks, title: 'Export of Feedback List', hostname });
     dbParams.client.close();
   }
   
