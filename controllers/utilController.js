fe2568b08c92a7bbf21b845ef168b1002220b7bf
innova1 <tom@teaminnovationllc.com>
2019-01-04 11:26:41 -0500

    cleaning up

--- controllers/utilController.js
+++ controllers/utilController.js
@@ -2,33 +2,33 @@ const { MongoClient, ObjectId } = require('mongodb');
 const debug = require('debug')('app:utilController');
 
 exports.setupDB = async function () {
-  const url = process.env.DB_URL;
-  debug(`attempting to connect to database at ${url}`);
-  const dbName = 'tasks';
-  try {
-    const client = await MongoClient.connect(url, { useNewUrlParser: true });
-    const db = client.db(dbName);
-    const collection = await db.collection('tasks');
-    return ({ client: client, collection: collection })
-  }
+    const url = process.env.DB_URL;
+    debug(`attempting to connect to database at ${url}`);
+    const dbName = 'tasks';
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true });
+        const db = client.db(dbName);
+        const collection = await db.collection('tasks');
+        return ({ client: client, collection: collection });
+    }
 
-  catch (err) {
-    debug(err);
-  }
+    catch (err) {
+        debug(err);
+    }
 };
 
 exports.setupUserDB = async function () {
-  const url = process.env.DB_URL;
-  debug(`attempting to connect to database at ${url}`);
-  const dbName = 'tasks';
-  try {
-    const client = await MongoClient.connect(url, { useNewUrlParser: true });
-    const db = client.db(dbName);
-    const collection = await db.collection('users');
-    return ({ client: client, collection: collection })
-  }
+    const url = process.env.DB_URL;
+    debug(`attempting to connect to database at ${url}`);
+    const dbName = 'tasks';
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true });
+        const db = client.db(dbName);
+        const collection = await db.collection('users');
+        return ({ client: client, collection: collection });
+    }
 
-  catch (err) {
-    debug(err);
-  }
+    catch (err) {
+        debug(err);
+    }
 };
