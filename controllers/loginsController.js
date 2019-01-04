a80399abd42fd4bb8cbb6619b8c1477d0437a5a0
innova1 <tom@teaminnovationllc.com>
2019-01-04 13:45:30 -0500

    Update loginsController.js

--- controllers/loginsController.js
+++ controllers/loginsController.js
@@ -2,6 +2,13 @@ const { MongoClient, ObjectId } = require('mongodb');
 const util = require('./utilController');
 const debug = require('debug')('app:loginsController');
 
+function hash(pwd, salt) {
+    //crypto.DEFAULT_ENCODING = 'hex'; <-- causes typeerror and db connection freeze up until app restart
+    const key = crypto.pbkdf2Sync(pwd, salt, 100000, 64, 'sha512');
+    debug("hashed string is " + key.toString('hex'));  // '3745e48...08d59ae'
+    return key.toString('hex');
+}
+
 exports.loginPage = (req, res) => {
     redirectUrl = req.cookies.redirectUrl;
     debug("redirectUrl from cookie is " + redirectUrl);
@@ -12,6 +19,7 @@ exports.login = async (req, res) => {
 
     const user = req.body;
     const fbkor = user.fbkor;
+    const email = user.email;
     const fbkorPwd = user.fbkorPwd;
     const redirectUrl = user.redirectUrl;
     temploginAttempt = parseInt(user.loginAttempt);
@@ -30,12 +38,16 @@ exports.login = async (req, res) => {
         compare with localhash above
         if match then true and go to redirectUrl else false and return to login page with "not match" message
         */
-        const foundUser = await dbParams.collection.findOne( { emailname: "tomboulet@gmail.com" } );
+        const foundUser = await dbParams.collection.findOne( { emailname: email } );
 
         //get dbhash and salt out of user. do localhash and compare and redirect as needed
-        const dbPwd = foundUser.password;
+        const dbPwdHash = foundUser.password;
+        const salt = foundUser.salt;
+        
+        //hash it
+        const fbkorPwdHash = hash(fbkorPwd, salt);
 
-        if( dbPwd == fbkorPwd ) {
+        if( dbPwdHash == fbkorPwdHash ) {
             debug("entered pwd " + fbkorPwd + " is the same as db password " + dbPwd + ", about to redirect to " + redirectUrl);
             //set this cookie only if a password check works
             res.cookie('username', fbkor, { expires: new Date(Date.now() + (30 * 24 * 60 * 60 * 1000)) });
