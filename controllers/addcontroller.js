762ea0fa8b15c05fba25c206be753f6104f553fb
innova1 <tom@teaminnovationllc.com>
2019-01-04 12:39:20 -0500

    Update addcontroller.js

--- controllers/addcontroller.js
+++ controllers/addcontroller.js
@@ -1,6 +1,19 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const util = require('./utilController');
+const crypto = require('crypto');
 const debug = require('debug')('app:addController');
+
+function grindSalt() {
+    crypto.randomBytes(64, (err, buf) => {
+        if (err) throw err;
+        debug(`${buf.length} bytes of random data: ${buf.toString('hex')}`);
+    });
+}
+
+function hash(pwd, salt) {
+    const key = crypto.pbkdf2Sync(pwd, salt, 100000, 64, 'sha512');
+    debug(key.toString('hex'));  // '3745e48...08d59ae'
+}
 
 exports.addTask = (req, res) => {
     res.render('addTask', { title: 'Adding some feedback' });
@@ -13,7 +26,6 @@ exports.addFbkx = (req, res) => {
 };
 
 exports.addFbk = (req, res) => {
-    //res.cookie('username', 'josephine', {});
     const un = req.cookies.username;
     const shortname = un.split(",")[0]
     const email = un.split(",")[1]
@@ -21,20 +33,20 @@ exports.addFbk = (req, res) => {
 };
 
 exports.saveTask = async (req, res) => {
-  try {
-    var tempfbk = req.body;
-    tempfbk.fbkee = { 'shortname': tempfbk.fbkee.split(",")[0], 'email': tempfbk.fbkee.split(",")[1] };
-    tempfbk.fbkor = { 'shortname': tempfbk.fbkor.split(",")[0], 'email': tempfbk.fbkor.split(",")[1] };
-    const task = req.body;
-    const dbParams = await util.setupDB();
-    await dbParams.collection.insertOne(task);
-    dbParams.client.close();
-    res.redirect('/');
-  }
+    try {
+        var tempfbk = req.body;
+        tempfbk.fbkee = { 'shortname': tempfbk.fbkee.split(",")[0], 'email': tempfbk.fbkee.split(",")[1] };
+        tempfbk.fbkor = { 'shortname': tempfbk.fbkor.split(",")[0], 'email': tempfbk.fbkor.split(",")[1] };
+        const task = req.body;
+        const dbParams = await util.setupDB();
+        await dbParams.collection.insertOne(task);
+        dbParams.client.close();
+        res.redirect('/');
+    }
 
-  catch(err) {
-    debug(err);
-  }
+    catch(err) {
+        debug(err);
+    }
 };
 
 exports.addUserPage = async (req, res) => {
@@ -42,12 +54,14 @@ exports.addUserPage = async (req, res) => {
 };
 
 exports.addUser = async (req, res) => {
-    try {
         tempUser = req.body;
         var password = tempUser.password;
         //add salt and hash password
-        tempUser.salt = "some calc'd value";
-        tempUser.password = "hashed password here";
+        tempUser.salt = grindSalt();
+        hashed = hash(password);
+        debug("took " + password + " and hashed to " + hashed);
+        tempUser.password = hashed;
+    try {
         const user = tempUser; //doing this because it seems these variables need to be const for some reason
         const dbParams = await util.setupUserDB();
         await dbParams.collection.insertOne(user);
