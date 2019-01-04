ec8df2975bc6caaf61067e6b30a734670dc96954
innova1 <tom@teaminnovationllc.com>
2019-01-03 22:36:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,10 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redi
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/public/stylesheets'));
 app.use('/webfonts', express.static(__dirname + '/public/fonts/webfonts/')); 
+
+app.use('*', checkLogin, function(req, res, next) {
+    next();
+});
 app.use('/export', checkLogin, function(req, res, next) {
     next();
 });
