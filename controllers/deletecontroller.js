83d23bec13c7085df7a0aa014734f7735c44a9c2
innova1 <tom@teaminnovationllc.com>
2019-01-04 11:52:32 -0500

    cleanup

--- controllers/deletecontroller.js
+++ controllers/deletecontroller.js
@@ -3,30 +3,30 @@ const util = require('./utilController');
 const debug = require('debug')('app:deleteController');
 
 exports.deleteTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const dbParams = await util.setupDB();
-    const task = await dbParams.collection.findOne({ _id: new ObjectId(id) });
-    dbParams.client.close();
-    res.render('confirmDelete', { task, title: 'Confirm Delete' });
-  }
+    try {
+        const { id } = req.params;
+        const dbParams = await util.setupDB();
+        const fbk = await dbParams.collection.findOne({ _id: new ObjectId(id) });
+        dbParams.client.close();
+        res.render('confirmDelete', { fbk, title: 'Confirm Delete' });
+    }
 
-  catch (err) {
-    debug(err);
-  }
+    catch (err) {
+        debug(err);
+    }
 }
 
 exports.confirmDelete = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const dbParams = await util.setupDB();
-    const task = await dbParams.collection.deleteOne({ _id: new ObjectId(id) });
-    const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
-    dbParams.client.close();
-    res.redirect('/');
-  }
+    try {
+        const { id } = req.params;
+        const dbParams = await util.setupDB();
+        const fbk = await dbParams.collection.deleteOne({ _id: new ObjectId(id) });
+        //const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
+        dbParams.client.close();
+        res.redirect('/');
+    }
 
-  catch (err) {
-    debug(err);
-  };
+    catch (err) {
+        debug(err);
+    };
 };
