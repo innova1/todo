f76a0e26d67dc81a83b52c33b3c5564dcd7a5aaa
innova1 <tom@teaminnovationllc.com>
2019-01-06 00:38:32 -0500

    Update deletecontroller.js

--- controllers/deletecontroller.js
+++ controllers/deletecontroller.js
@@ -1,6 +1,15 @@
 const { ObjectId } = require('mongodb');
 const util = require('./utilController');
+const log4js = require('log4js');
 const debug = require('debug')('app:deleteController');
+
+log4js.configure({
+  appenders: { 'useractivity': { type: 'file', filename: 'user.log', layout: { type: 'pattern', pattern: '%d %X{ip} %m%n' } } },
+  categories: { default: { appenders: ['useractivity'], level: 'info' } }
+});
+
+var logger = log4js.getLogger(); 
+logger.level = 'info';
 
 exports.deleteTask = async (req, res) => {
     try {
@@ -17,10 +26,12 @@ exports.deleteTask = async (req, res) => {
 };
 
 exports.confirmDelete = async (req, res) => {
+    logger.addContext('ip', req.ip);
     try {
         const { id } = req.params;
         const dbParams = await util.setupDB();
         const fbk = await dbParams.collection.deleteOne({ _id: new ObjectId(id) });
+        logger.info("deleted fbk: " + email);
         //const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
         dbParams.client.close();
         res.redirect('/');
