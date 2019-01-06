f99ad4194bd2e6b244acf5f49075660f95c51d92
innova1 <tom@teaminnovationllc.com>
2019-01-05 22:38:11 -0500

    Update logController.js

--- controllers/logController.js
+++ controllers/logController.js
@@ -2,8 +2,10 @@ const log4js = require('log4js');
 const debug = require('debug')('app:LogController');
 
 var logger = log4js.getLogger(); //test
+logger.level = 'info';
 
 exports.testLog = (req, res) => {
     console.log('Hello');
+    logger.info('info from log4js');
     res.render('logTest', { title: 'Testing logging' });
 };
