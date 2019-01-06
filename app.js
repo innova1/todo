27d4e682628c63742a9fa6be4ebaa9a0fe573e0c
innova1 <tom@teaminnovationllc.com>
2019-01-05 22:29:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function isLoggedIn(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(morgan('tiny'));
+app.use(morgan('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
