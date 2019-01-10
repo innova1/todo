545bda87fb1d7ea3aa80079deeb3a4d29a161246
innova1 <tom@teaminnovationllc.com>
2019-01-09 20:07:50 -0600

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -32,7 +32,7 @@ exports.getScore = async function(email) {
 
 exports.setRating = async (req, res) => {
     try {
-        debug("in set rating");
+        debug("in set rating. params 0:" + req.params[0] + ", 1:" + req.params[1] + ", 2:" + req.params[2] + ", 3:" + req.params[3]);
         const rating = req.query.rating;
         const id = req.query.id;
         debug("updating id: " + id + " with rating " + rating);
