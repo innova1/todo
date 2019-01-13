b35719622c73d49df888995c32281f8dcbedefb0
innova1 <tom@teaminnovationllc.com>
2019-01-12 20:59:42 -0500

    Update editController.js

--- controllers/editController.js
+++ controllers/editController.js
@@ -33,7 +33,7 @@ exports.fixDate = async (req, res) => {
     try {
         const { id } = req.params;
         const fbk = req.body;
-        task.createdDate = new Date(fbk.dueDate);
+        fbk.createdDate = new Date(fbk.dueDate);
         const dbParams = await util.setupDB();
         await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, fbk);
         dbParams.client.close();
