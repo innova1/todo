d4a0914777c6c0c64c7cce0bf3c36490f201068f
innova1 <tom@teaminnovationllc.com>
2019-01-14 16:51:57 -0600

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -241,7 +241,7 @@ exports.getScoreboard = async function() {
         const secondDate = new Date();
         const diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
         
-        debug("diffDays: " + diffDays);
+        debug("firstDate: " + firstDate.getDate() + ", secondDate: " + secondDate.getDate() + ", diffDays: " + diffDays);
         
         const allUserFbksOutAgg = await dbParams.collection.aggregate( [
             {
