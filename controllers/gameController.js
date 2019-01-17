3951d3306e2807152574574619c50b938729df3d
innova1 <tom@teaminnovationllc.com>
2019-01-17 09:48:11 -0600

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -212,14 +212,20 @@ exports.getAvgScores = async function(email) {
         ] ); 
         
         let aggInArr = await fbkInAgg.toArray();
-        if(typeof fbkInArr === 'undefined' ) {
-            debug("fbkInArr is undefined");
+        let aggOutArr = await fbkOutAgg.toArray();
+        
+        if( (typeof fbkInArr === 'undefined' || typeof fbkOutArr === 'undefined') || (email != 'tom.boulet@exxonmobil.com') { ) {
+            if (typeof fbkInArr === 'undefined') {
+                debug("fbkInArr is undefined");
+            } else if (typoeof fbkOutArr === 'undefined' ) {
+                debug("fbkOutArr is undefined");
+            } else if ( email != 'tom.boulet@exxonmobil.com') {
+                debug("not tom");
+            }
             return { inScore: 0, outScore: 0 };
         } else {
+   
             debug("fbk In: " + aggInArr[0].score); //JSON.stringify(aggInArr[0]));
-        
-        
-            let aggOutArr = await fbkOutAgg.toArray();
             debug("fbk out: " + aggOutArr[0].score); //JSON.stringify(aggOutArr[0]));
 
             aggOutArr.forEach( (doc) => {
