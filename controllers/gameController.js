32057ceacd0331c6a0d3216f6608d60a5b8e648a
innova1 <tom@teaminnovationllc.com>
2019-01-18 23:03:23 -0500

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -311,7 +311,7 @@ exports.getScoreboard = async function() {
                 $sort: { 'createDate': -1 }
             },
             {
-                $addFields: { outRating: { $toInt: "$rating"}, fbkoremail: "$fbkor.email", todayDate: new Date() }
+                $addFields: { outRating: { $toInt: "$rating"}, fbkoremail: "$fbkor.email", todayDate: secondDate }
             },
             {   $group: { 
                     _id: {  fbkoremail: "$fbkoremail" , month: { $month: "$createDate" }, day: { $dayOfMonth: "$createDate" }, year: { $year: "$createDate" } },
