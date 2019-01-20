236350a2ba38bd7f58d86a5e246de1c4b9dff835
innova1 <tom@teaminnovationllc.com>
2019-01-20 01:11:01 -0500

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -351,6 +351,7 @@ exports.getScoreboard = async function() {
             {
                 $project: {
                     _id: { fbkoremail: "$fbkoremail" },
+                    shortemail: { $substrBytes [ "$fbkoremail", 0, { indexOfBytes: [ "$fbkoremail" ] } ] },
                     a: "$avgOutPerDay",
                     s: "$sumAllOutRating",
                     totalOut: "$totalOutFbks", 
