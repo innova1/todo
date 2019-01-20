17226f2a8e85b368ef5406f0553c290aaa1fd472
innova1 <tom@teaminnovationllc.com>
2019-01-19 22:06:54 -0500

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -381,7 +381,7 @@ exports.getScoreboard = async function() {
             c++;
         });
         
-        debug("after forEach loop: " + outputArray[0].noRating.isNoRatingIn);
+        await debug("after forEach loop: " + outputArray[0].noRating.isNoRatingIn);
         return await outputArray;
         dbParams.client.close();
         
