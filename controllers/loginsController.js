6c415629e5a1602ef8fa1ef90ca3dbf89c757d6d
innova1 <tom@teaminnovationllc.com>
2019-02-16 21:59:47 -0500

    Update loginsController.js

--- controllers/loginsController.js
+++ controllers/loginsController.js
@@ -30,7 +30,7 @@ exports.login = async (req, res) => {
     const user = req.body;
     debug("email before was " + user.email);
     const email = user.email.trim().toLowerCase();;
-    debug("email is now" + email);
+    debug("email is now " + email);
     const userPwd = user.userPwd;
     const redirectUrl = user.redirectUrl;
     temploginAttempt = parseInt(user.loginAttempt);
